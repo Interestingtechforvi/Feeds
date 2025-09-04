f42e17e7770d2120f019cdfc38beb5a2cab39dc1
Interestingtechforvi <143540752+Interestingtechforvi@users.noreply.github.com>
2025-09-04 17:33:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,12 +25,13 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # Gemini API Keys
 GEMINI_API_KEYS = [
         "AIzaSyBlh2kqJti4Fi9h-nJOzgPDMHDpXCGB0jo",
+        "AIzaSyBlh2kqJti4Fi9h-nJOzgPDMHDpXCGB0jo",
     "AIzaSyDPL6XUmT9g7OYQdwdXZHMjAAmmGzK_v0Y",
         "AIzaSyBmrlGFb5ABO5hkPS8xtjD-JqnRwUv9Z6U",
     "AIzaSyDYeLxp7Jp5qSypbVBPy9v_XYmz7Sc1qfs",
     "AIzaSyBBU-ImEZhezaUnUq71h60Tfs0H0XWXiDY",
-    "AIzaSyAioecEQqNGoQzkhQM6L2G1Zn8bn-xRVxg"
-]
+    "AIzaSyAioecEQqNGoQzkhQM6L2G1Zn8bn-xRVxg",
+"AIzaSyBlh2kqJti4Fi9h-nJOzgPDMHDpXCGB0jo"]
 # Available Gemini TTS Voices
 GEMINI_VOICES = [
     "Zephyr", "Puck", "Charon", "Kore", "Fenrir", "Leda",
